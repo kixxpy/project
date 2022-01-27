e5a41f47c963e65cac87b04f6174858d36986c88
kixx <ekn62@bk.ru>
2022-01-27 23:32:39 +0300

    qwqw22

--- script.js
+++ script.js
@@ -10,12 +10,32 @@ const personalMoviDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = +prompt('На сколько оцните его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = +prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = +prompt('На сколько оцните его?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = +prompt('На сколько оцените его?', '');
 
-personalMoviDB.movies[a] = b;
-personalMoviDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцните его?', '');
+        
+    if ( a != '' && b != '' && a != null && b != null && a.length < 50 && b.length < 50) {
+        personalMoviDB.movies[a] = b;
+        console.log('yes');
+    } else {
+        console.log('ne');
+        i--;
+    }
 
+}
 console.log(personalMoviDB);
+
+if (personalMoviDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMoviDB.count > 10 && personalMoviDB.count <30) {
+    console.log('Вы классный зритель!');
+} else if (personalMoviDB.count > 30) {
+    console.log('Вы киноман!!!');
+} else {
+    console.log('Произошла ошибка');
+}
